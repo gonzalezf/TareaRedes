6afb23a6ab2355830afff49819c816f37477b66d
Ignacio Tolosa <ignacio.tolosa.12@sansano.usm.cl>
2015-03-24 17:15:28 -0300

    Add files

---
+++ IndexHandler.java
@@ -0,0 +1,20 @@
+
+class IndexHandler {
+
+   IndexHandler (String name) {
+   }
+
+   public void run() {
+    out.write("HTTP/1.1 200 OK\r\n");
+    out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+    out.write("Server: Apache/0.8.4\r\n");
+    out.write("Content-Type: text/html\r\n");
+    out.write("Content-Length: 59\r\n");
+    out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
+    out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+    out.write("\r\n");
+    out.write("<TITLE>Hola</TITLE>");
+    out.write("<P>Pagina bienvenida!</P>");
+    out.write("\n\n");
+   }
+}
--- server.java
+++ server.java
@@ -1,24 +1,87 @@
 import java.io.*;
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-class server
+class Server
 {
-	public static void main(String argv[]) throws Exception
-      {
-		Integer port = 8080;
-        String clientSentence;
-        String capitalizedSentence;
-        ServerSocket welcomeSocket = new ServerSocket(port);
+  public Server() {
+    ThreadPool this.pool = new ThreadPool(5, 10);
 
-         while(true)
-         {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Receivewerwerd: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
-         }
-      }
+      String line = "This order was placed for QT3000! OK?";
+      String pattern = "(POST|GET) ([^ ]*) (HTTP.*)";
+
+      // Create a Pattern object
+      Pattern this.r = Pattern.compile(pattern);
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    // création de la socket
+    int port = 8080;
+    ServerSocket serverSocket = new ServerSocket(port);
+    System.err.println("Serveur lancé sur le port : " + port);
+
+    // repeatedly wait for connections, and process
+    while (true) {
+        // on reste bloqué sur l'attente d'une demande client
+        Socket clientSocket = serverSocket.accept();
+        System.err.println("Nouveau client connecté");
+
+        this.pool.execute(new IndexHandler());
+
+        // on ouvre un flux de converation
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+        // chaque fois qu'une donnée est lue sur le réseau on la renvoi sur
+        // le flux d'écriture.
+        // la donnée lue est donc retournée exactement au même client.
+        String s;
+        int i = 0;
+        Boolean exitflag = false;
+        while ((s = in.readLine()) != null) {
+            if (i == 0) {
+              Matcher m = this.r.matcher(s);
+              if (m.find( )) {
+                String method = m.group(0);
+                String url = m.group(1);
+                String httpv = m.group(2);
+              } else {
+                System.out.println("501 Bad request");
+                out.write("HTTP/1.1 501 Bad request\r\n\n");
+                out.close();
+                in.close();
+                clientSocket.close();
+                exitflag = true;
+              }
+            }
+            System.out.println(s);
+            if (s.isEmpty()) {
+                break;
+            }
+        }
+
+        if (exitflag) {
+          continue;
+        }
+
+        if (url == "/") {
+          this.pool.execute(new IndexHandler(method, url, httpv));
+        } else if (url == "/home_old") {
+          this.pool.execute(new HomeOldHandler(method, url, httpv));
+        } else if (url == "/secret") {
+          this.pool.execute(new SecretHandler(method, url, httpv));
+        } else {
+          // error
+        }
+
+        // on ferme les flux.
+        System.err.println("Connexion avec le client terminée");
+        out.close();
+        in.close();
+        clientSocket.close();
+    }
+  }
 }
